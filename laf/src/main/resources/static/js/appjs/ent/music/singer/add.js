dc3b7cbe4fdc5f71ed3106773d99260268afcfde
lafhuang <lafhuang@gmail.com>
2019-04-18 18:11:18 +0800

    login page

--- laf/src/main/resources/static/js/appjs/ent/music/singer/add.js
+++ laf/src/main/resources/static/js/appjs/ent/music/singer/add.js
@@ -1,12 +1,34 @@
 $().ready(function() {
 
-    $('#birthday').datepicker({
-        dateFormat : 'yy-mm-dd',
-        prevText : '<i class="fa fa-chevron-left"></i>',
-        nextText : '<i class="fa fa-chevron-right"></i>',
-        onSelect : function(selectedDate) {
+    $.fn.datepicker.dates['cn'] = {   //切换为中文显示
+        days: ["周日", "周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+        daysShort: ["日", "一", "二", "三", "四", "五", "六", "七"],
+        daysMin: ["日", "一", "二", "三", "四", "五", "六", "七"],
+        months: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+        monthsShort: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+        today: "今天",
+        clear: "清除"
+    };
 
-        }
+    $('#birthday').datepicker({
+        autoclose: true, //自动关闭
+        beforeShowDay: $.noop,    //在显示日期之前调用的函数
+        calendarWeeks: false,     //是否显示今年是第几周
+        clearBtn: false,          //显示清除按钮
+        daysOfWeekDisabled: [],   //星期几不可选
+        endDate: Infinity,        //日历结束日期
+        forceParse: true,         //是否强制转换不符合格式的字符串
+        format: 'yyyy-mm-dd',     //日期格式
+        keyboardNavigation: true, //是否显示箭头导航
+        language: 'cn',           //语言
+        minViewMode: 0,
+        orientation: "auto",      //方向
+        rtl: false,
+        startDate: -Infinity,     //日历开始日期
+        startView: 0,             //开始显示
+        todayBtn: false,          //今天按钮
+        todayHighlight: false,    //今天高亮
+        weekStart: 0              //星期几是开始
     });
 
     $('#star').rating({
@@ -24,7 +46,91 @@ $().ready(function() {
 
     $(".fileinput-remove-button").click(function() {
         $("#photo").val("");
-    })
+    });
+
+
+    var $singerForm = $("#singerForm").validate({
+        errorClass		: errorClass,
+        errorElement	: errorElement,
+        highlight: function(element) {
+            $(element).parent().removeClass('state-success').addClass("state-error");
+            $(element).removeClass('valid');
+        },
+        unhighlight: function(element) {
+            $(element).parent().removeClass("state-error").addClass('state-success');
+            $(element).addClass('valid');
+        },
+        // Rules for form validation
+        rules : {
+            name : {
+                required : true
+            },
+            enName : {
+                required : true
+            },
+            region : {
+                required : true
+            },
+            initial : {
+                required : true
+            },
+            birthday : {
+                required : true
+            },
+            type : {
+                required : true
+            },
+            star : {
+                required : true
+            },
+            photo : {
+                required : true
+            }
+        },
+
+        // Messages for form validation
+        messages : {
+            name : {
+                required : ''
+            },
+            enName : {
+                required : ''
+            },
+            region : {
+                required : ''
+            },
+            initial : {
+                required : ''
+            },
+            birthday : {
+                required : ''
+            },
+            type : {
+                required : ''
+            },
+            star : {
+                required : ''
+            },
+            photo : {
+                required : ''
+            }
+        },
+
+        // Ajax form submition
+        submitHandler : function(form) {
+            $(form).ajaxSubmit({
+                success : function() {
+                    $("#comment-form").addClass('submited');
+                }
+            });
+        },
+
+        // Do not change code below
+        errorPlacement : function(error, element) {
+            error.insertAfter(element.parent());
+        }
+    });
+
 });
 
 function initFileUpload() {
