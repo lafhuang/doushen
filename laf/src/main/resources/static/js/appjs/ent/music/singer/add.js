2506cbc0875bd2373639c8d5f7f844d43fd06570
lafhuang <lafhuang@gmail.com>
2019-04-18 22:26:33 +0800

    singer add page update

--- laf/src/main/resources/static/js/appjs/ent/music/singer/add.js
+++ laf/src/main/resources/static/js/appjs/ent/music/singer/add.js
@@ -36,31 +36,35 @@ $().ready(function() {
         min: 0,
         max: 5,
         step: 1,
-        stars: 5,
-        starCaptions: {1: '就那样', 2: '可以听听', 3: '还不错', 4: '喜欢', 5: '超爱'}
+        stars: 5
     });
 
     loadDict();
 
-    initFileUpload();
+    var errorClass = 'invalid';
+    var errorElement = 'em';
 
-    $(".fileinput-remove-button").click(function() {
-        $("#photo").val("");
-    });
-
-
-    var $singerForm = $("#singerForm").validate({
+    var $singerForm = $('#singerForm').validate({
         errorClass		: errorClass,
         errorElement	: errorElement,
         highlight: function(element) {
-            $(element).parent().removeClass('state-success').addClass("state-error");
-            $(element).removeClass('valid');
+            if ("star" == $(element).attr("name")) {
+                $(element).parent().parent().parent().removeClass('state-success').addClass("state-error");
+                $(element).removeClass('valid');
+            } else {
+                $(element).parent().removeClass('state-success').addClass("state-error");
+                $(element).removeClass('valid');
+            }
         },
         unhighlight: function(element) {
-            $(element).parent().removeClass("state-error").addClass('state-success');
-            $(element).addClass('valid');
+            if ("star" == $(element).attr("name")) {
+                $(element).parent().parent().parent().removeClass("state-error").addClass('state-success');
+                $(element).addClass('valid');
+            } else {
+                $(element).parent().removeClass("state-error").addClass('state-success');
+                $(element).addClass('valid');
+            }
         },
-        // Rules for form validation
         rules : {
             name : {
                 required : true
@@ -87,69 +91,51 @@ $().ready(function() {
                 required : true
             }
         },
-
-        // Messages for form validation
         messages : {
             name : {
-                required : ''
+                required : '请输入歌手姓名'
             },
             enName : {
-                required : ''
+                required : '请输入歌手英文名'
             },
             region : {
-                required : ''
+                required : '请选择歌手所在地区'
             },
             initial : {
-                required : ''
+                required : '请选择歌手首字母'
             },
             birthday : {
-                required : ''
+                required : '请选择歌手出生日期'
             },
             type : {
-                required : ''
+                required : '请选择歌手类型'
             },
             star : {
-                required : ''
+                required : '请选择歌手星级'
             },
             photo : {
-                required : ''
+                required : '请上传歌手图片'
             }
         },
-
-        // Ajax form submition
         submitHandler : function(form) {
             $(form).ajaxSubmit({
                 success : function() {
-                    $("#comment-form").addClass('submited');
+                    save();
                 }
             });
         },
-
-        // Do not change code below
         errorPlacement : function(error, element) {
-            error.insertAfter(element.parent());
+            if ("star" == element.attr("name")) {
+                error.insertAfter(element.parent().parent().parent());
+            } else {
+                error.insertAfter(element.parent());
+            }
         }
     });
 
 });
 
-function initFileUpload() {
-    $("#img").fileinput({
-        language : 'zh',
-        uploadUrl : "/upload/music_singer",
-        showPreview: false,
-        allowedFileExtensions: ["jpg", "jpeg", "gif", "png"],
-        elErrorContainer: "#errorBlock"
-    }).on("fileuploaded", function(e, data) {//文件上传成功的回调函数，还有其他的一些回调函数，比如上传之前...
-        var res = data.response;
-        if (res.code == '0') {
-            $("#photo").val(res.msg);
-        }
-    });
-}
-
 function save() {
-    // validate
 
 	$.ajax({
 		cache : true,
@@ -158,17 +144,13 @@ function save() {
 		data : $('#singerForm').serialize(),
 		async : false,
 		error : function(request) {
-			parent.layer.alert("Connection error");
+			alert("Connection error");
 		},
 		success : function(data) {
 			if (data.code == 0) {
-				parent.layer.msg("操作成功");
-				parent.reLoad();
-				var index = parent.layer.getFrameIndex(window.name); // 获取窗口索引
-				parent.layer.close(index);
-
+				alert(data.msg)
 			} else {
-				parent.layer.alert(data.msg)
+				alert(data.msg)
 			}
 
 		}
@@ -196,11 +178,11 @@ function load_singer_dict(dict_type) {
         },success : function(result) {
             //加载数据
             if ("singer_initial" == dict_type) {
-                html += '<option value="">请选择歌手首字母</option>'
+                html += '<option selected="" disabled="">歌手首字母</option>'
             } else if ("singer_region" == dict_type) {
-                html += '<option value="">请选择歌手所在地区</option>'
+                html += '<option selected="" disabled="">歌手所在地区</option>'
             } else if ("singer_type" == dict_type) {
-                html += '<option value="">请选择歌手类型</option>'
+                html += '<option selected="" disabled="">歌手类型</option>'
             }
             for (var i = 0; i < result.length; i++) {
                 html += '<option value="' + result[i].dictValue + '">' + result[i].dictName + '</option>'
@@ -212,4 +194,8 @@ function load_singer_dict(dict_type) {
 
 function goBack(target) {
     getTarget(target);
+}
+
+function uploadFile(file) {
+    alert(file);
 }
