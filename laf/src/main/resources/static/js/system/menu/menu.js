65c1b0e36a9e2e00df16f5fe0916e823802acc43
lafhuang <lafhuang@gmail.com>
2019-07-08 23:50:28 +0800

    menu list update

--- laf/src/main/resources/static/js/system/menu/menu.js
+++ laf/src/main/resources/static/js/system/menu/menu.js
@@ -1,23 +1,32 @@
-var request_prefix = "/system/menu"
+var request_prefix = "/system/menu";
+
 $(document).ready(function () {
     load();
 });
+
 var load = function () {
     $('#exampleTable')
         .bootstrapTreeTable(
             {
+                rootIdValue: '0',
                 id: 'id',
-                code: 'id',
-                parentCode: 'parentId',
+                parentId: 'parentId',
                 type: "GET", // 请求数据的ajax类型
                 url: request_prefix + '/list', // 请求数据的ajax的url
                 ajaxParams: {sort:'sort'}, // 请求数据的ajax的data属性
-                expandColumn: '0',// 在哪一列上面显示展开按钮
-                striped: true, // 是否各行渐变色
-                bordered: true, // 是否显示边框
+                expandColumn: '1',// 在哪一列上面显示展开按钮
                 expandAll: false, // 是否全部展开
+                expandFirst : true,
+                toolbar: null,
+                striped: true, // 是否各行渐变色
+                hover: true,
+                bordered: true, // 是否显示边框
+                checkbox: true,
                 singleSelect : false, // 设置为true将禁止多选
                 columns: [
+                    {
+                        checkbox: true
+                    },
                     {
                         title: '名称',
                         field: 'menuName'
@@ -25,28 +34,39 @@ var load = function () {
                     {
                         title: '图标',
                         field: 'icon',
-                        formatter: function (item, index) {
-                            return item.icon == null ? '' : '<i class="' + item.icon + ' fa-lg"></i>';
+                        formatter: function (value, row, index) {
+                            return row.icon == null ? '' : '<i class="' + row.icon + ' fa-lg"></i>';
                         }
+                    },
+                    {
+                        title: '排序',
+                        field: 'sort'
                     },
                     {
                         title: '类型',
                         field: 'type',
-                        formatter: function (item, index) {
-                            if (item.type === '0') {
-                                return '<span class="label label-primary">目录</span>';
+                        formatter: function (value, row, index) {
+                            if (row.type === '0') {
+                                return '目录';
                             }
-                            if (item.type === '1') {
-                                return '<span class="label label-success">菜单</span>';
+                            if (row.type === '1') {
+                                return '菜单';
                             }
-                            if (item.type === '2') {
-                                return '<span class="label label-warning">按钮</span>';
+                            if (row.type === '2') {
+                                return '按钮';
                             }
                         }
                     },
                     {
                         title: '地址',
-                        field: 'url'
+                        field: 'url',
+                        formatter: function (value, row, index) {
+                            if (value == null || value == "") {
+                                return "#";
+                            } else {
+                                return value;
+                            }
+                        }
                     },
                     {
                         title: '权限标识',
@@ -56,32 +76,33 @@ var load = function () {
                         title: '操作',
                         field: 'id',
                         align : 'center',
-                        formatter: function (item, index) {
+                        formatter: function (value, row, index) {
                             var e = '<a class="btn btn-primary btn-sm '
                                 + s_edit_h
                                 + '" href="#" mce_href="#" title="编辑" onclick="edit(\''
-                                + item.id
+                                + row.id
                                 + '\')"><i class="fa fa-edit"></i></a> ';
 
                             var p = '';
-                            if (item.type === '0' || item.type === '1') {
+                            if (row.type === '0' || row.type === '1') {
                                 p += '<a class="btn btn-primary btn-sm '
                                     + s_add_h
                                     + '" href="#" mce_href="#" title="添加子节点" onclick="add(\''
-                                    + item.id
+                                    + row.id
                                     + '\')"><i class="fa fa-plus"></i></a> ';
                             }
 
                             var d = '<a class="btn btn-warning btn-sm '
                                 + s_remove_h
                                 + '" href="#" title="删除"  mce_href="#" onclick="remove(\''
-                                + item.id
+                                + row.id
                                 + '\')"><i class="fa fa-remove"></i></a> ';
 
                             return e + d + p;
                         }
                     }]
             });
+    $(".treetable-bars").hide();
 }
 
 function reLoad() {
@@ -89,14 +110,7 @@ function reLoad() {
 }
 
 function add(pId) {
-    layer.open({
-        type: 2,
-        title: '增加菜单',
-        maxmin: true,
-        shadeClose: false, // 点击遮罩关闭层
-        area: ['60%', '60%'],
-        content: request_prefix + '/add/' + pId // iframe的url
-    });
+    getTarget(request_prefix + '/add/' + pId);
 }
 
 function remove(id) {
